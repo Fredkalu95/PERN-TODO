6ed9f340409e43c657731cdc8a4fcfff7dac123e
fred-kalu-bptn <110822175+fred-kalu-bptn@users.noreply.github.com>
2023-07-15 23:32:53 +0200

    more fixes

--- client/src/components/ListTodos.tsx
+++ client/src/components/ListTodos.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { EditTodo } from "./EditTodo";
 
 interface IListTodos {

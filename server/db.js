fde14caa7479ec610a80a68ec8b81d98b88443ea
fred-kalu-bptn <110822175+fred-kalu-bptn@users.noreply.github.com>
2023-07-15 23:56:22 +0200

    change

--- server/db.js
+++ server/db.js
@@ -1,11 +1,15 @@
 const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "",
+//     host: "pern-todo-steel.vercel.app",
+//     port: 5432,
+//     database: "perntodo"
+// });
+
 const pool = new Pool({
-    user: "postgres",
-    password: "",
-    host: "pern-todo-steel.vercel.app",
-    port: 5432,
-    database: "perntodo"
-});
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  })
 
 module.exports = pool;

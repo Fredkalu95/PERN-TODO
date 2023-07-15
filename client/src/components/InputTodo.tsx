981853d20a47bf8a46bf3118ba521cf0d7e60844
Fred Kalumba <fredkalumba@gmail.com>
2023-07-15 23:24:27 +0200

    Update InputTodo.tsx

--- client/src/components/InputTodo.tsx
+++ client/src/components/InputTodo.tsx
@@ -8,7 +8,7 @@ export const InputTodo = () => {
     e.preventDefault();
     try {
       const body : { description: string } = {description};
-      const response = await fetch("http://localhost:5000/todos", {
+      const response = await fetch("pern-todo-api.vercel.app:5000/todos", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
